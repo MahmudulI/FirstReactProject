a25b9f489d0ed38998a446cd52ba1fdb8a7c05f8
Mahmudul <mahmudulislamapon724@gmail.com>
2025-10-26 04:32:18 -0700

    first

---
+++ React-project/src/Components/Root/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Root.css"
+const Root = () => {
+  return (
+    <div>
+        
+       <h1> I am form Root.jsx file</h1>
+    </div>
+  )
+}
+
+export default Root
